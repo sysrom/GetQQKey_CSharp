22691c434d2e5147f8c140df6d8c422dcc863b2d
sysrom <88297660+sysrom@users.noreply.github.com>
2023-01-16 23:17:55 +0800

    Upload

---
+++ CSharpGUI/QQkeyDLLDEMO/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace QQkeyDLLDEMO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 62);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "取QQ号";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(268, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 62);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "取ClientKey号";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(396, 10);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 62);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "取HttpKey号";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(140, 10);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(122, 62);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "取QQ名";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(523, 84);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "GetQQInfo By sysR@M";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Close);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ CSharpGUI/QQkeyDLLDEMO/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Reflection; 
+using System.Reflection.Emit;
+using System.Diagnostics;
+
+namespace QQkeyDLLDEMO
+{
+    public partial class Form1 : Form
+    {
+        
+        string DLLpath = Path.Combine(Application.StartupPath, @"BigJB.dll");
+        public Form1()
+        {
+            if (!File.Exists(DLLpath))
+            {
+                File.WriteAllBytes(DLLpath, Properties.Resources.QQkeyDll);
+            }
+            InitializeComponent();
+        }
+        [DllImport("BigJB.dll", EntryPoint = "GetClientUin")]
+        static extern string GetClientUin(int pid);
+        [DllImport("BigJB.dll", EntryPoint = "GetCookies")]
+        static extern string GetCookies(int qqnum,string qqkey);
+        [DllImport("BigJB.dll", EntryPoint = "GetClientKey")]
+        static extern string GetClientKey(int pid);
+        [DllImport("BigJB.dll", EntryPoint = "GetClientName")]
+        static extern string GetClientName(long qqnum,int pid);
+        [DllImport("BigJB.dll", EntryPoint = "GetHttpKey")]
+        static extern string GetHttpKey(int pid);
+        public static int GetTIMpid() {
+            Process[] all = Process.GetProcessesByName("TIM");
+            int pid;
+            pid = all[0].Id;
+            return pid;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("By sysR@M\n QQ号:"+GetClientUin(7072)+" QQ名:"+GetClientName(Convert.ToInt32(GetClientUin(7072)),7072)+"\nQQClientKey:"+GetClientKey(7072)+"\nQQHttpKey:"+GetHttpKey(7072));
+            //MessageBox.Show(GetOnepid("TIM.exe").ToString());
+            MessageBox.Show("By sysR@M\nQQ号:"+GetClientUin(GetTIMpid()));
+        }
+
+        private void Close(object sender, FormClosedEventArgs e)
+        {
+            File.Delete(DLLpath);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("By sysR@M\nQQ名:"+ GetClientName(long.Parse(GetClientUin(GetTIMpid())), GetTIMpid()));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("By sysR@M\nClientKey:"+GetClientKey(GetTIMpid()));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("By sysR@M\nHttpKey:"+GetHttpKey(GetTIMpid()));
+        }
+    }
+}
